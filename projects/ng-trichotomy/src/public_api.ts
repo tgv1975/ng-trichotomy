080cfcd2f7eb556bc9738abbe073fa9a02addf09
TGV <tgv1975@yahoo.com>
2020-04-16 07:31:27 +0300

    Update to Angular 9.1.2

--- projects/ng-trichotomy/src/public_api.ts
+++ projects/ng-trichotomy/src/public_api.ts
@@ -5,3 +5,4 @@ export * from './lib/ng-trichotomy.module';
 export * from './lib/trichotomy/ng-trichotomy.service';
 export * from './lib/trichotomy/ng-trichotomy.component';
 export * from './lib/trichotomy-bar/ng-trichotomy-bar.component';
+export * from './lib/trichotomy-centerpiece/ng-trichotomy-centerpiece.component';
